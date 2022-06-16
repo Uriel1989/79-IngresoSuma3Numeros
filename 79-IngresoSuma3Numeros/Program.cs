221c3cffe5e23720de2cda8112dea0fda0132052
Uriel Guillermo Daniel <45216219+Uriel1989@users.noreply.github.com>
2022-06-15 21:47:41 -0300

    Add files via upload

---
+++ 79-IngresoSuma3Numeros/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+/*Crear 3 variables numéricas con el valor que tu quieras
+ * en otra variable numérica guardar el valor de la suma de las 3 anteriores. 
+ * Mostrar por consola.
+ */
+
+namespace _79_IngresoSuma3Numeros
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingresar 3 numeros para sumar:");
+
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+            int c = Convert.ToInt32(Console.ReadLine());
+
+            int d = (a + b + c);
+
+            Console.WriteLine("Resultado:" + d);
+        }
+    }
+}
